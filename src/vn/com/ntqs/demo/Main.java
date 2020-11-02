131f3f613fb75c7d7c7cf2135a29e155cc146b16
Hoàng Tuấn <tuan.hoang2@ntq-solution.com.vn>
2020-11-02 16:04:38 +0700

    Implement simple +/- operations

--- src/vn/com/ntqs/demo/Main.java
+++ src/vn/com/ntqs/demo/Main.java
@@ -1,10 +1,45 @@
 package vn.com.ntqs.demo;
 
+import java.io.Console;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Main {
 
+	private static PrintStream log = System.out;
+	private static Scanner cs = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
+		log.println("!~ SIMPLE CALCULATOR ~!");
+		log.println("Choose arithmetic operations (+ or -):");
+		
+		String operation = cs.nextLine();
+		if (operation.equals("+")) {
+			doAdd();
+		} else if (operation.equals("-")) {
+			doSub();
+		} else {
+			log.println("Not supported!");
+		}
+		
 	}
-
+	
+	public static void doAdd() {
+		log.println("Enter 1st number:");
+		String num1 = cs.nextLine();
+		log.println("Enter 2nd number:");
+		String num2 = cs.nextLine();
+		
+		log.format("The ADD result: %s + %s = %d", num1, num2, Integer.parseInt(num1) + Integer.parseInt(num2));
+	}
+	
+	public static void doSub() {
+		log.println("Enter 1st number:");
+		String num1 = cs.nextLine();
+		log.println("Enter 2nd number:");
+		String num2 = cs.nextLine();
+		
+		log.format("The SUB result: %s - %s = %d", num1, num2, Integer.parseInt(num1) - Integer.parseInt(num2));
+	}
 }
